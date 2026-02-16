72ad0e10601f911fa3a6971a4fcfe6848541d968
edgar <edgar.ad.martinez@gmail.com>
2026-02-16 10:11:43 -0500

    Saving sessions to psql instance

--- main.py
+++ main.py
@@ -1,15 +1,94 @@
-from typing import Annotated, Callable, Optional
+from contextlib import asynccontextmanager
+from datetime import date, datetime
+from enum import Enum
+from typing import Annotated, TypeVar
 from agents import Runner, function_tool, Agent, WebSearchTool
 from dotenv import load_dotenv
 import os
 import asyncio
 from fastapi import FastAPI
-from pydantic import BaseModel
+from psycopg.rows import class_row 
+from pydantic import BaseModel, TypeAdapter
+import psycopg
 
-load_dotenv()
+T = TypeVar("T", covariant=True)
+
+_ = load_dotenv()
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 assert OPENAI_API_KEY is not None, "Missing API Key"
+
+conn_string = os.environ.get("DB_CONN_STRING")
+assert conn_string is not None, "Missing PG connection string"
+
+class RoleEnum(str, Enum):
+    user = 'user'
+    assistant = 'assistant'
+
+class SessionRow(BaseModel):
+    session_id: str
+    role: RoleEnum
+    content: str
+    created_at: datetime
+
+SessionRowsValidator = TypeAdapter(list[SessionRow])
+
+async def init_db():
+
+    print("Initializing DB")
+
+    assert conn_string is not None, "Missing PG connection string"
+    async with await psycopg.AsyncConnection.connect(conninfo=conn_string) as conn:
+
+        async with conn.cursor() as cur:
+
+            print("Creating sessions table")
+            _ = await conn.execute("""
+            CREATE TABLE IF NOT EXISTS public.sessions (
+                session_id VARCHAR,
+                role VARCHAR,
+                content VARCHAR,
+                created_at TIMESTAMP
+            )
+            """)
+            print("Sessions table created")
+
+
+async def get_session(session_id: str) -> list[SessionRow]:
+    assert conn_string is not None, "Missing PG connection string"
+    async with await psycopg.AsyncConnection.connect(conninfo=conn_string) as conn:
+
+        async with conn.cursor(row_factory=class_row(SessionRow)) as cur:
+
+            _ = await cur.execute("""
+            SELECT
+                session_id,
+                role,
+                content,
+                created_at
+            FROM sessions
+            WHERE session_id = %s
+            ORDER BY created_at ASC;
+            """, (session_id,))
+
+            session_rows = await cur.fetchall()
+
+        return session_rows
+
+async def save_session(session_row: SessionRow) -> None:
+
+    assert conn_string is not None, "Missing PG connection string"
+    async with await psycopg.AsyncConnection.connect(conninfo=conn_string) as conn:
+
+        async with conn.cursor(row_factory=class_row(SessionRow)) as cur:
+
+            _ = await cur.execute("""
+            INSERT INTO sessions (session_id, role, content, created_at)
+            VALUES ( %s, %s, %s, %s );
+            """, (session_row.session_id, session_row.role, session_row.content, session_row.created_at))
+
+        return
+
 
 @function_tool
 async def test_tool(a: Annotated[str, "Test variable a"], bb: Annotated[str, "test var bbb"], ccc: int = 3) -> str:
@@ -17,16 +96,33 @@ async def test_tool(a: Annotated[str, "Test variable a"], bb: Annotated[str, "te
 
     return "hey"
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 class PromptBody(BaseModel):
     text: str
-    session_id: Optional[str] = None
+    session_id: str
+
 
 @app.post("/prompt")
 async def post_prompt(prompt: PromptBody):
 
     print("Hello from ai-assistant!")
+    latest_chat = SessionRow(
+        session_id=prompt.session_id,
+        role=RoleEnum.user,
+        content=prompt.text,
+        created_at=datetime.now()
+    )
+
+    session_data = await get_session(prompt.session_id)
+
+    prompt.text = f"Previous context: {session_data},\nUser: { prompt.text }"
 
     agent = Agent(
         name="assistant",
@@ -36,7 +132,16 @@ async def post_prompt(prompt: PromptBody):
     print(f"agent_func_schemas: { agent.tools }")
 
     result = await Runner.run(agent, prompt.text)
+    agent_chat = SessionRow(
+        session_id=prompt.session_id,
+        role=RoleEnum.assistant,
+        content=str(result.final_output),
+        created_at=datetime.now()
+    )
     print(f"Responses: { result.raw_responses }")
+
+    await save_session(latest_chat)
+    await save_session(agent_chat)
 
     return {"response": result.final_output}
 
